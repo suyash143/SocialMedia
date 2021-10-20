6c9a48f4f1f323d789b4bef22aec09969f3f550f
Suyash pathak <63799666+suyash143@users.noreply.github.com>
2021-10-20 16:50:22 +0530

    Update settings.py

--- LINKEDIN/settings.py
+++ LINKEDIN/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^r)o3cp=tc)_^55$_7&-)i!0*=zcxe230^9p@5_q9)j-s9ze7k'
+SECRET_KEY = 'django-insecure-^r)o3cp='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
